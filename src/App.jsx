dc398561327a0c78fd24edd0f40aa6ef164ae925
SwadebDeb sharma <swadebriva2012@gmail.com>
2024-11-27 01:43:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Countries from "./components/Countries";
+
+
+const App = () => {
+  return (
+    <div>
+     <Countries></Countries>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+
+
+const Countries = () => {
+      return (
+            <div>
+                 <h2>World Tour</h2> 
+            </div>
+      );
+};
+
+export default Countries;
